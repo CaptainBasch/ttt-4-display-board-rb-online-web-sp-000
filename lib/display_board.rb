c7376433ebb52fc6af0a9578127b132e2edd89f6
Bailey Davis <bailey.gage.davis@gmail.com>
2020-05-04 15:47:20 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(game_state)
+  puts "#{game_state[0]}"
+  puts "-----------"
   puts ""
-  puts ""
-  puts ""
-  puts ""
+  puts "-----------"
   puts ""
 end
